e5e0b2cef3ec2a3fdf5581e7fe40f3bf1c79c00b
Niksune <47556429+Niksune@users.noreply.github.com>
2022-08-04 14:18:46 +0200

    First commit

---
+++ src/main/java/net/niksune/ResourceTrading/ResourceTradingApplication.java
@@ -0,0 +1,13 @@
+package net.niksune.ResourceTrading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceTradingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourceTradingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/niksune/ResourceTrading/ResourceTradingApplicationTests.java
@@ -0,0 +1,13 @@
+package net.niksune.ResourceTrading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceTradingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
